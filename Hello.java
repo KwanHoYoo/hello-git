c205707a7627fa9c5f30adab45590237e6a0417a
kwan ho <ykh4343@naver.com>
2023-10-07 14:51:23 +0000

    C4: modify hello.java

--- Hello.java
+++ Hello.java
@@ -6,5 +6,7 @@
 	 public static void main(String[] args) {
  		 System.out.println("Hello, world from GIT!");
  		 System.out.println("Changes after First commit!");
-          }
+        
 
+	 	System.out.println("Changes after Pushing to remote!");
+	 }
